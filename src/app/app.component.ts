85936afe7a1833d56f3b44a02af769eebae8f311
ANKIT KUMAR GUPTA <65163759+ankit-kumar-guptaa@users.noreply.github.com>
2024-11-18 23:35:12 +0530

    login_done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isLoggedIn: boolean = true;
+  isLoggedIn: boolean = false;
   loggedInUserName: string = '';
 
   constructor(private router: Router) {}
