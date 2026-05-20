93ffb6175c671cf3b1ea99f0e10e414673566502
ANKIT KUMAR GUPTA <65163759+ankit-kumar-guptaa@users.noreply.github.com>
2026-05-20 23:35:17 +0530

    feat: implement admin login, header, and login-signup UI components

--- src/app/components/login-signup/login-signup.component.ts
+++ src/app/components/login-signup/login-signup.component.ts
@@ -18,7 +18,7 @@ export class LoginSignupComponent implements OnInit {
   progress: number = 0;
   resendTimer: number = 0;
   currentOtp: string = '';
-  domain: string = 'delhi.gov.in';
+  domain: string = 'airliquide.com';
   serverOTP: string = '';
   serverPhoneOTP: string = '';
   _user: UserModel = new UserModel();
